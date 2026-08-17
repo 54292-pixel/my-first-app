e19218e23931fbe16f8ab3caa9fd8ae8f219d883
54292-pixel <54292@yupparaj.ac.th>
2026-08-17 13:30:12 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -52,6 +52,7 @@ def show_result_dialog(ans1, ans2, ans3, ans4):
         st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
 
     # ✏️ [พื้นที่สำหรับนักเรียน]: เพิ่มตรวจข้อ 3, 4 ตรงนี้
+
 if u_ans3 == "banana":
         st.success("✅ ข้อ 3: ถูกต้อง")
         score += 1
