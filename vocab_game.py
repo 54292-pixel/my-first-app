a0af126eebc990168dc5f3b51543f8adebb85c5f
54292-pixel <54292@yupparaj.ac.th>
2026-08-17 13:27:55 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -55,13 +55,13 @@ def show_result_dialog(ans1, ans2, ans3, ans4):
 if u_ans3 == "banana":
         st.success("✅ ข้อ 3: ถูกต้อง")
         score += 1
-    else:
+else:
         st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
 
 if u_ans4 == "lemon":
         st.success("✅ ข้อ 4: ถูกต้อง")
         score += 1
-    else:
+else:
         st.error(f"❌ ข้อ 4: ยังไม่ถูกต้อง (คุณตอบ '{u_ans4}')")
 
 
